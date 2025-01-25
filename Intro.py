ad730bbb6908dc3fe362784eef1132fb734ac0dc
anupam <sanupam2004@gmail.com>
2025-01-25 11:59:09 +0530

    Introduction

--- Intro.py
+++ Intro.py
@@ -1,3 +1,36 @@
 # Python is a General Purpose Programming Language
 # Has a Large standard Library
-# For Data science 
+
+print('Hello, Python!') 
+
+import sys 
+print(sys.version)
+
+# type() shows us the data type of the input
+
+print(type(12))
+
+# Type Casting
+print(float(10))
+print(int(10.5))
+
+# Type casting the int inside the string gives out the int inside the string unlike c++ in which we had to subtract '0' to get the actual int inside the string
+print(int('10'))
+
+# The code below gives an error 
+# print(int('A'))
+
+
+# int and float can be converted into Strings
+
+print(str(20.5))
+
+# In boolean Values it Is necessary to use Uppercase T and F in True and False
+# int Value of 1 is True and of 0 is False and vice-versa
+
+# this ->  // is used for Int division ,It rounds up the result
+
+
+
+
+
